56a606f79bf41d06674a51b069faa591878116cd
Jhonatan Laguna <hola@jhonatanlaguna.com>
2017-03-06 22:47:23 -0700

    Tarea para JS agregada

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,18 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect'); //Crea un servidor de desarrollo local
 var open = require('gulp-open'); //Abre una URL en el navegador
+var browserify = require('browserify'); //Empaqueta nuesto JS
+var reactify = require('reactify'); // Transdorma React JSX en JS
+var source = require('vinyl-source-stream'); //Permite usar texto convencional en Gulp
 
 var config = {
     port: 9005,
     devBaseUrl: 'http://localhost',
     paths: {
         html: './src/*.html',
-        dist: './dist'
+        js: './src/**/*.js',
+        dist: './dist',
+        mainJs: './src/main.js'
     }
 };
 
@@ -36,10 +41,21 @@ gulp.task('html', function () {
         .pipe(connect.reload());
 });
 
+gulp.task('js', function () {
+    browserify(config.paths.mainJs)
+        .transform(reactify)
+        .bundle()
+        .on('error', console.error.bind(console))
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest(config.paths.dist + '/scripts'))
+        .pipe(connect.reload());
+});
+
 //Revisa si hay cambios en los archivos para actualizar el navegador
 gulp.task('watch', function () {
     gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.js, ['js']);
 });
 
 //Comando default, llama a los demas
-gulp.task('default', ['html', 'open', 'watch']);
+gulp.task('default', ['html', 'js', 'open', 'watch']);
---
+++ src/main.js
@@ -0,0 +1,7 @@
+/**
+ * Created by laguna on 06/03/2017.
+ */
+
+var App = console.log('Hola desde Broserify');
+
+module.exports = App;
